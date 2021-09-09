d66a06b60b27f2d2aee8d38d1ccd532c576c47aa
Jordan-624 <89818947+Jordan-624@users.noreply.github.com>
2021-09-09 16:57:38 -0600

    Add files via upload

---
+++ birthYear.py
@@ -0,0 +1,11 @@
+# Figure out users birth year.
+
+birth_year = input("What is your birth year? ")
+
+# Find out an equation to figure out their approximate age.
+
+age = 2021 - int(birth_year)
+
+# Give user ouput.
+
+print("You are around",age,"years old!")
